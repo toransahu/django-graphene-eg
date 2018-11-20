9130d11c8fb21df7f2662e09bfc5dac1284ebec0
Toran Sahu <toran.sahu@yahoo.com>
2018-11-20 09:58:32 +0530

    [fixed] provided on_delete param

--- src/ingredients/models.py
+++ src/ingredients/models.py
@@ -52,4 +52,4 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published = models.BooleanField(default=False)
-    owner = models.ForeignKey("auth.User")
+    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE)
