3806df93c4d6dba884a6603e39dd508a24fade97
Toran Sahu <toran.sahu@yahoo.com>
2018-11-20 18:11:06 +0530

    [new] plan to add serializer to consume in graph

---
+++ src/ingredients/serializers.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-20 12:31
+
+"""
+serializers.py
+"""
+
+
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
